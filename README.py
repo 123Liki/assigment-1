2a5e610b7579eeb465ca556b06d5652886fd72e8
123Liki <98078313+123Liki@users.noreply.github.com>
2022-01-20 18:49:25 +0530

    Add files via upload
    C:\Users\B.LIKITHA\Downloads\assigment.py

---
+++ README.py
@@ -0,0 +1,18 @@
+# assigment-1
+
+print("BANOTH LIKITHA  ASSIGMENT-1")
+print(int(45.5))
+print(float(56))
+print(bool(50))
+dict={'cat':'pet','lion':'animal','color':'balck'}
+print(dict.keys())
+print(dict)
+t=(1,5,3,6)
+print('Type of t is',type(t))
+l=[4,6,8,10]
+print('Length of list is',len(l))
+set={'12','python'}
+print(set)
+print(len(set))
+set.add('APPLE')
+print(set)
